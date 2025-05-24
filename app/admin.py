8c6eae3257d9e69fa732bd56bbdcf9dd62f3cab2
limbanga <95570833+limbanga@users.noreply.github.com>
2025-05-24 23:25:42 +0700

    Add BookingAdmin class to customize admin interface for Booking model

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import *
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'showtime', 'colored_status', 'expired_at', 'total_amount')
+    list_filter = ('status', 'showtime')
+    search_fields = ('user__username', 'showtime__movie__title')
+    readonly_fields = ('expired_at',)
+    
+    def colored_status(self, obj):
+        color = {
+            'pending': 'orange',
+            'confirmed': 'green',
+            'cancelled': 'red',
+            'expired': 'gray',
+        }.get(obj.status, 'black')
+        return format_html(
+            '<span style="color: {};">{}</span>',
+            color,
+            obj.status.capitalize()
+        )
+    colored_status.short_description = 'Status'
+    colored_status.admin_order_field = 'status'
 
 
 # Register your models here.
@@ -12,6 +34,6 @@ admin.site.register(Room)
 admin.site.register(SeatType)
 admin.site.register(Seat)
 admin.site.register(Showtime)
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(BookingSeat)
 admin.site.register(Payment)
